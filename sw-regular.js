67b9cb4318a2657637cfaff2bb0605fa33b2b192
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-07-28 09:46:43 +0000

    Update app version to v3.3.1

--- sw-regular.js
+++ sw-regular.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const APP_VERSION  = "v3.3.0";
+const APP_VERSION  = "v3.3.1";
 const CACHE_NAME   = `model-aptitude-test2-${APP_VERSION}`;
 
 /* ====================
